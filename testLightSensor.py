61ed67c858fd6ee810000a8a6292565a1023181e
toai-nguyen <maxngudan@gmail.com>
2024-09-27 08:21:43 +0700

    test commit

--- testLightSensor.py
+++ testLightSensor.py
@@ -1,29 +1,20 @@
 import RPi.GPIO as GPIO
-
 import time
 
-#GPIO SETUP
-
-channel = 4
-
+# Thiết lập GPIO
 GPIO.setmode(GPIO.BCM)
+channel = 17  # Sử dụng GPIO 17 cho ví dụ
 
 GPIO.setup(channel, GPIO.IN)
 
 def callback(channel):
+    print("Phát hiện thay đổi trạng thái!")
 
-        if GPIO.input(channel):
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
 
-                print ("Water Detected!")
-
-        else:
-
-                print ("Water Detected!")
-
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  
-
-GPIO.add_event_callback(channel, callback)  
-
-while True:
-
-        time.sleep(0)
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
